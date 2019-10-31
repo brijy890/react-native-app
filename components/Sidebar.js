a0504d9fb632a80320984b9f38057db41ffa7b0a
brij <brijbhushan.y@pepperfry.com>
2019-10-31 19:37:03 +0530

    react-native app

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: '#000' }}>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
