a42ce9eca20b737fe93fff9385b46b5ba1bf60ce
brij <brijbhushan.y@pepperfry.com>
2019-11-01 15:20:31 +0530

    drawer complete

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
+import {
+  Container,
+  Content,
+  Text,
+  Button,
+  Footer,
+  FooterTab
+} from 'native-base';
 
 export default class Sidebar extends Component {
   render() {
     return (
-      <View style={{ backgroundColor: '#000' }}>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        <Content>
+          <Image
+            source={require('../../assets/main.jpg')}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Text>Sign Up</Text>
+            </Button>
+            <Button>
+              <Text>Login</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
